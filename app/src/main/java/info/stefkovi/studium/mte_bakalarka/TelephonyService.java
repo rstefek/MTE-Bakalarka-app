4427a5e3451733cae27ae01c1131ea772f6b6033
Roman Štefek <roman@stefkovi.info>
2023-10-14 15:30:51 +0200

    Prototyp zjišťování informací o buňce

--- app/src/main/java/info/stefkovi/studium/mte_bakalarka/TelephonyService.java
+++ app/src/main/java/info/stefkovi/studium/mte_bakalarka/TelephonyService.java
@@ -1,16 +1,32 @@
 package info.stefkovi.studium.mte_bakalarka;
 
+import android.telephony.CellInfo;
+import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
+
+import java.util.List;
 
 public class TelephonyService {
 
     private TelephonyManager _manager;
+    private CellInfoListener _cellListener;
+
+    private static class CellInfoListener extends PhoneStateListener {
+        List<CellInfo> cellInfo;
+        @Override
+        public void onCellInfoChanged(List<CellInfo> cellInfo) {
+            this.cellInfo = cellInfo;
+        }
+    }
 
     public TelephonyService(TelephonyManager manager) {
         this._manager = manager;
+        _cellListener = new CellInfoListener();
+        _manager.listen(_cellListener, PhoneStateListener.LISTEN_CELL_INFO);
     }
 
     public String GetBasicDeviceInfo() {
         return "";
     }
+
 }
