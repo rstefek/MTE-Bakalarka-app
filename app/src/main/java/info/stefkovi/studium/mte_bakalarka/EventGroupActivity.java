a8cfbcd8eb255b814b57f576d18fda74a2b77dd8
Roman Štefek <roman@stefkovi.info>
2023-11-03 07:40:14 +0100

    Doplnění obrazovky na správu event grup

---
+++ app/src/main/java/info/stefkovi/studium/mte_bakalarka/EventGroupActivity.java
@@ -0,0 +1,15 @@
+package info.stefkovi.studium.mte_bakalarka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_group);
+    }
+
+}
